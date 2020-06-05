e559f03587b70dc0cdad72574febeaeb8715b600
Cameron Lares <cameronlares@gmail.com>
2020-06-04 23:22:27 -0400

    Almost MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,138 @@
     https://api.github.com/users/<your name>
 */
 
+// const URL = 'https://api.github.com/users/cameronlares';
+
+// axios.get(URL)
+// .then(response=> console.log(response))
+// .catch(error=>  console.log('Error',error ))
+
+
+
+
+{/* <div class="card">
+      <img src={image url of user} />
+      <div class="card-info">
+        <h3 class="name">{users name}</h3>
+        <p class="username">{users user name}</p>
+        <p>Location: {users location}</p>
+        <p>Profile:
+          <a href={address to users github page}>{address to users github page}</a>
+        </p>
+        <p>Followers: {users followers count}</p>
+        <p>Following: {users following count}</p>
+        <p>Bio: {users bio}</p>
+      </div>
+    </div> */}
+    // const followersArray = ['https://api.github.com/users/cameronlares',
+    // 'https://api.github.com/users/tetondan',
+    // 'https://api.github.com/users/dustinmyers',
+    // 'https://api.github.com/users/justsml',
+    // 'https://api.github.com/users/luishrd',
+    // 'https://api.github.com/users/bigknell'
+    
+    // ];
+
+    const followersArray = ['cameronlares',
+    'tetondan',
+    'dustinmyers',
+    'justsml',
+    'luishrd',
+    'bigknell'
+    
+    ];
+
+ const fetchUsers = () => {
+  axios.get(`https://api.github.com/users/${followersArray[0]}`)
+  // axios.get('https://api.github.com/users/cameronlares')
+
+      .then(response => {
+
+          const users = response.data;
+
+      //Function
+
+      const cardCreator= function (arr){
+        const card = document.createElement('div') // class of card
+        const img = document.createElement('img') //scr 
+        const cardInfo=document.createElement('div')
+         // nested
+          const name=document.createElement('h3') //  name class
+        const userName=document.createElement('p')// username class
+        const myLocation=document.createElement('p')
+        const profile=document.createElement('p')
+         const href=document.createElement('a') // src href nested in p tag profile
+         const followers=document.createElement('p')
+         const following = document.createElement('p')
+         const bio=document.createElement('p')
+        
+         //Add Classes
+         card.classList.add('card')
+        // img.setAttribute('src','users.avatar_url')
+        name.classList.add('name')
+        userName.classList.add('username')
+        href.setAttribute('href','')
+        
+        
+        //Add Order
+        
+        card.appendChild(img)
+        card.appendChild(cardInfo)
+        cardInfo.appendChild(name)
+        cardInfo.appendChild(userName)
+        cardInfo.appendChild(myLocation)
+        cardInfo.appendChild(profile)
+        profile.appendChild(href)
+        cardInfo.appendChild(followers)
+        cardInfo.appendChild(following)
+        cardInfo.appendChild(bio)
+      
+      //
+      
+      img.src=arr.avatar_url
+      name.textContent= arr.name  
+      userName.textContent=arr.login
+      myLocation.textContent=`Location: ${arr.location}`
+      href.href=arr.html_url
+      followers.textContent=`Followers: ${arr.followers}`
+      following.textContent=`Following: ${arr.following}`
+      bio.textContent=`Bio: ${arr.bio}`
+        
+        return card
+      
+      }
+      
+      const myCard = document.querySelector('.cards')
+      const whatever = cardCreator(users)
+
+      
+      myCard.appendChild(whatever)
+// console.log(users)
+
+      followersArray.forEach(arr=>{
+
+        console.log(arr)
+        })
+      
+     
+   
+ 
+//  })
+
+         console.log(users)
+     
+
+         
+      })
+      .catch(error => console.error(error));
+};
+
+
+fetchUsers()
+
+
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +160,7 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
